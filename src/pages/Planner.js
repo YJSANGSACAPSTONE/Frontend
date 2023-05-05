65008cf9d467a1f2547e5526d52794693ed9d0f2
Ethereummom <103083686+Ethereummom@users.noreply.github.com>
2023-05-05 19:57:36 +0900

    d
    d

--- src/pages/Planner.js
+++ src/pages/Planner.js
@@ -81,7 +81,7 @@ function Planner(props){
 
 	// 일정 수정 이벤트 트리거
 	const updatePlan = () => {
-		let u_id = "sinsung@naver.com";
+		let u_id = "sanghee_ok@naver.com";
 		let u_title = $("input[name=u_title]").val();
 		let u_content = $("textarea[name=u_content]").val();
 		let u_category = $("select[name=u_category]").val();
@@ -235,7 +235,7 @@ function Planner(props){
 		
 		// 일정 입력 이벤트 트리거
 		const addPlan = () => {
-			let u_id = "sinsung@naver.com";
+			let u_id = "sanghee_ok@naver.com";
 			let p_title = $("input[name=p_title]").val();
 			let p_content = $("textarea[name=p_content]").val();
 			let p_category = $("select[name=p_category]").val();

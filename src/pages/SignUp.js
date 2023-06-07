9f679a9831234fad9d25bb994c4e368b4918e386
yuhyeon99 <jujini31@gmail.com>
2023-06-07 17:41:46 +0900

    feat : footer 스타일 수정

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -4,6 +4,12 @@ import $ from 'jquery';
 import Axios from "axios";
 import Cookies from 'js-cookie';
 function SignUp(props){
+    const jwtToken = Cookies.get("accessTokenCookie");
+    const refreshToken = Cookies.get("refreshTokenCookie");
+
+    console.log(jwtToken);
+    console.log(refreshToken);
+
 
     const history = useNavigate();
     const location = useLocation();
@@ -21,12 +27,21 @@ function SignUp(props){
         let u_zepid = "";
         let u_content = $("input[name=u_content]").val();
 
-        Axios.post("http://localhost:8070/user/adduser", 
+        console.log(u_id);
+        console.log(u_nickname);
+        console.log(u_zepid);
+        console.log(u_content);
+        
+        Axios.post("http://localhost:8070/user/adduser",
         {
-            u_id : u_id,
+            // u_id : u_id,
             u_nickname : u_nickname,
             u_zepid : u_zepid,
             u_content : u_content,
+        },{
+            headers: {
+                Authorization : `Bearer ${jwtToken}` 
+            }
         })
         .then(response=>{
             console.log(response);
@@ -74,8 +89,8 @@ function SignUp(props){
                             <li class="sign_form">
                                 <form action="">
                                     {/* <img src={userData.profileImageUrl} alt="" /> */}
-                                    <label htmlFor="signId">카카오아이디</label>
-                                    <input type="text" id="signId" placeHolder="@abcdefg.com"  name="u_id" />
+                                    {/* <label htmlFor="signId">카카오아이디</label>
+                                    <input type="text" id="signId" placeHolder="@abcdefg.com"  name="u_id" /> */}
                                     {/* <label htmlFor="signZep">ZEP 아이디</label>
                                     <input type="name" id="signZep" name="u_zepid" /> */}
                                     <label htmlFor="signName">닉네임</label>

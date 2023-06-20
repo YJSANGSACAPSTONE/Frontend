5eab3c7feb8da279998dfaa90aef751252e03bf6
yuhyeon99 <jujini31@gmail.com>
2023-06-20 17:41:33 +0900

    fix : 플래너 일정추가 오류 수정
    response 데이터 처리 수정

--- src/pages/Planner.js
+++ src/pages/Planner.js
@@ -195,7 +195,7 @@ function Planner(props){
 			Axios.get(`http://localhost:8070/plan/dailyplan?uid=${u_id}`)
 			.then((response) => {
 				console.log(response);
-				setPlans(response.data);
+				setPlans(response.data.list);
 				$("#listModal").css('display','none');
 			})
 			.catch(error => console.log(error));
@@ -263,8 +263,12 @@ function Planner(props){
 			})
 			.then(response=>{
 				console.log(response);
-				Axios.get(`http://localhost:8070/plan/dailyplan?uid=${u_id}`)
-				.then(response => setPlans(response.data))
+				Axios.get(`http://localhost:8070/plan/dailyplan`,{
+					headers : {
+						'Authorization': `Bearer ${jwtToken}`
+					}
+				})
+				.then(response => setPlans(response.data.list))
 				.catch(error => console.log(error));
 			})
 			.catch(error => {
@@ -449,9 +453,13 @@ function Planner(props){
 			})
 			.then(response=>{
 				console.log(response);
-				Axios.get(`http://localhost:8070/plan/dailyplan?uid=${u_id}`)
+				Axios.get(`http://localhost:8070/plan/dailyplan`,{
+					headers : {
+						'Authorization': `Bearer ${jwtToken}`
+					}
+				})
 				.then((response) => {
-					setPlans(response.data)
+					setPlans(response.data.list)
 					$("input[name=p_title]").val('');
 					$("textarea[name=p_content]").val('');
 					$("select[name=p_category]").val('');

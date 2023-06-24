1179b123eb86eea639ad0fc5bc7915c49e8703ad
yuhyeon99 <jujini31@gmail.com>
2023-06-24 18:03:06 +0900

    Update Challenge.js

--- src/pages/Challenge.js
+++ src/pages/Challenge.js
@@ -47,7 +47,7 @@ function Challenge(props){
                                         <ul>
                                             {Object.keys(popularlist).length > 0 ? (
                                                 popularlist.map(popularlist => (
-                                                    <li onClick={() => MvRead(popularlist)}><div><img src={`http://13.125.99.177:8070${popularlist.c_thumbnails}`} alt="morning"/><p>{popularlist.c_name}</p></div></li>
+                                                    <li onClick={() => MvRead(popularlist)}><div><img src={`https://godsaengplanner.com${popularlist.c_thumbnails}`} alt="morning"/><p>{popularlist.c_name}</p></div></li>
                                                 ))
                                             ) : (
                                                 <>
@@ -66,7 +66,7 @@ function Challenge(props){
                                         <ul>
                                             {Object.keys(recentlist).length > 0 ? (
                                                 recentlist.map(recentlist => (
-                                                    <li onClick={() => MvRead(recentlist)}><div><img src={`http://13.125.99.177:8070${recentlist.c_thumbnails}`} alt="morning"/><p>{recentlist.c_name}</p></div></li>
+                                                    <li onClick={() => MvRead(recentlist)}><div><img src={`https://godsaengplanner.com${recentlist.c_thumbnails}`} alt="morning"/><p>{recentlist.c_name}</p></div></li>
                                                 ))
                                             ) : (
                                                 <li><div><p>등록된 챌린지가 없습니다.</p></div></li>
@@ -83,7 +83,7 @@ function Challenge(props){
                                         <ul>
                                             {Object.keys(mylist).length > 0 ? (
                                                 mylist.map(mylist => (
-                                                    <li onClick={() => MvRead(mylist)}><div><img src={`http://13.125.99.177:8070${mylist.c_thumbnails}`} alt="morning"/><p>{mylist.c_name}</p></div></li>
+                                                    <li onClick={() => MvRead(mylist)}><div><img src={`https://godsaengplanner.com${mylist.c_thumbnails}`} alt="morning"/><p>{mylist.c_name}</p></div></li>
                                                 ))
                                             ) : (
                                                 <li><div><p>참여중인 챌린지가 없습니다.</p></div></li>

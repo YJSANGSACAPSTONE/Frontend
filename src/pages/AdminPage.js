164aa27780fc890fa4126abb274cc340a7dbf979
yuhyeon99 <jujini31@gmail.com>
2023-06-25 20:57:09 +0900

    Update AdminPage.js

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -109,8 +109,16 @@ function AdminPage(props){
             date.setDate(1); // 해당 월의 첫째 날로 설정
             const formattedDate = formatDate(date, 'YYYY-MM-DD'); // 날짜를 원하는 형식으로 포맷
             dateData.push(formattedDate); // x축 데이터를 담는 배열에 날짜를 push
-            randData.push(getRandomCount()); // 실제 차트 데이터에도 날짜와 랜덤한 인증 횟수를 push
+            // randData.push(getRandomCount()); // 실제 차트 데이터에도 날짜와 랜덤한 인증 횟수를 push
         }
+        
+        Axios.get(`http://localhost:8070/admin/statistic`)
+        .then((res)=>{
+            console.log(res);
+        })
+        .catch((err)=>{
+
+        });
         
         data.push(dateData);
         data.push(randData);

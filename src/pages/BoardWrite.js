b150947caf0e1b0082dbf8eb380d56ff9d148f0f
yuhyeon99 <jujini31@gmail.com>
2023-06-25 20:28:10 +0900

    Update BoardWrite.js

--- src/pages/BoardWrite.js
+++ src/pages/BoardWrite.js
@@ -15,8 +15,7 @@ function BoardWrite(props){
         po_content : "",
         po_secret : true,
         imageDTOList : [],
-        uploadFiles: null,
-        postphotofiles : [],
+        uploadFiles: null
     });
     
 
@@ -38,102 +37,41 @@ function BoardWrite(props){
         navigate('/board');
     }
     
-    const boardSubmit = async () => {      
-
-        try {
-
-            const postRequestData = board;
-            const selectedFile = board.uploadFiles;
-
-            const res = await Axios.get('/api/s3upload/s3', {
-              params: { size : selectedFile.length }
-            });
-            console.log(res.data);
-            const encodedFileName = res.data.encodedFileName;
-            const preSignedUrl = res.data.preSignedUrl;
-            
-            console.log("encodedFileName : "+encodedFileName);
-            console.log("presignedUrl : "+preSignedUrl);
-            
-            selectedFile.map(async (file, i)=> {
-                postRequestData.postphotofiles.append(encodedFileName[i]);
-                await Axios.put(preSignedUrl[i], file, {
-                    headers: {
-                        'Content-Type': file.type
-                    }
-                }); 
-            });
-
-            console.log("이미지 업로드 성공");
-
-            Axios.post("/api/post/register", postRequestData)
-            .then((res) => {
-                navigate("/board");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-
-            
-            
-            
-            
-            
-            // await Axios.put(preSignedUrl, selectedFile, {
-            //     headers: {
-            //         'Content-Type': selectedFile.type
-            //     }
-            // });
-            // console.log('이미지 업로드 완료');
-
-            
+    const boardSubmit = () => {
+        const formData = new FormData();
       
-            // console.log(challengeData);
-      
-            // await Axios.post('/api/challenge/addchallenge', challengeData);
-      
-            // console.log('챌린지 등록 완료');
-            // history('/challenge');
-          } catch (error) {
-            console.error('이미지 업로드 오류:', error);
+        if (board.uploadFiles != null) {
+          for (let i = 0; i < board.uploadFiles.length; i++) {
+            formData.append("uploadFiles", board.uploadFiles[i]);
           }
-
-
-
-        // if (board.uploadFiles != null) {
-        //   for (let i = 0; i < board.uploadFiles.length; i++) {
-        //     formData.append("uploadFiles", board.uploadFiles[i]);
-        //   }
       
-        //   Axios.post('/api/uploadAjax', formData)
-        //     .then((res) => {
-        //       console.log(res);
-        //       const path = "/img/boardimgtemp/" + res.data[0].folderPath;
-        //       const unifiedPath = path.replace(/\\/g, "/");
-        //       const uuid = res.data[0].uuid;
-        //       const fileName = uuid + "_" + res.data[0].fileName;
+          Axios.post('/api/uploadAjax', formData)
+            .then((res) => {
+              console.log(res);
+              const path = "/img/boardimgtemp/" + res.data[0].folderPath;
+              const unifiedPath = path.replace(/\\/g, "/");
+              const uuid = res.data[0].uuid;
+              const fileName = uuid + "_" + res.data[0].fileName;
       
-        //       setBoard(prevState => {
-        //         const updatedImageDTOList = [...prevState.imageDTOList, { path: unifiedPath, imgName: fileName, uuid: uuid }];
+              setBoard(prevState => {
+                const updatedImageDTOList = [...prevState.imageDTOList, { path: unifiedPath, imgName: fileName, uuid: uuid }];
                 
-        //         // 사진 업로드가 완료된 후에 아래 코드 실행
-        //         performAdditionalActions(updatedImageDTOList);
+                // 사진 업로드가 완료된 후에 아래 코드 실행
+                performAdditionalActions(updatedImageDTOList);
       
-        //         return {
-        //           ...prevState,
-        //           imageDTOList: updatedImageDTOList
-        //         };
-        //       });
-        //     })
-        //     .catch((err) => {
-        //       console.log(err);
-        //     });
-
-        
-        // } else {
-        //   // 사진이 없는 경우 바로 아래 코드 실행
-        //   performAdditionalActions([]);
-        // }
+                return {
+                  ...prevState,
+                  imageDTOList: updatedImageDTOList
+                };
+              });
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        } else {
+          // 사진이 없는 경우 바로 아래 코드 실행
+          performAdditionalActions([]);
+        }
       };
 
     // 사진 업로드 이후에 실행할 추가 작업을 수행하는 함수
